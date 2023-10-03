7199fe3ff6e7b93cb3a0ff43ffafca1d41c674a6
haffienc <haffienoorcahyo@gmail.com>
2023-10-03 23:43:18 +0700

    Tugas 5

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from main.views import show_main, create_item, show_xml, show_json, show_xml_by_
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_item
+from main.views import delete_item
 
 app_name = 'main'
 
@@ -16,4 +18,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-item/<int:id>', edit_item, name='edit_item'),
+    path('delete/<int:id>', delete_item, name='delete_item'),
 ]
